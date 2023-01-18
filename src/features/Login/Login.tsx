8e5cb8b352711ed6fa91bba77a7cf1aa91452f24
quangla1 <quangla1@viettel.com.vn>
2023-01-18 14:25:19 +0700

    fix

--- src/features/Login/Login.tsx
+++ src/features/Login/Login.tsx
@@ -1,9 +1,13 @@
 import { Checkbox } from "antd";
-import React from "react";
+import { Formik } from "formik";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./style.scss";
 
 function Login() {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
   return (
     <div className="container-fluid login-page">
       <div className="login-page-content">
@@ -31,19 +35,32 @@ function Login() {
             <span>or</span>
             <hr />
           </div>
-          <div className="content-right-login-form">
-            <div className="login-input-text">
-              <input type="text" placeholder="Email address" />
-            </div>
-            <div className="login-input-text">
-              <input type="password" placeholder="Password" />
-            </div>
-          </div>
-          <div className="content-right-remember-check">
-            <Checkbox>Remember me</Checkbox>
-            <Link to="/forgot-password">Forgot your password?</Link>
-          </div>
-          <button className="btn-login">Login</button>
+          <Formik
+            initialValues={{ email: email, password: password }}
+            onSubmit={(values) => {
+              setEmail(values.email);
+              setPassword(values.password);
+              console.log("Email:", email, password);
+            }}
+          >
+            {({ values, errors, handleChange, handleSubmit }) => (
+              <form onSubmit={handleSubmit}>
+                <div className="content-right-login-form">
+                  <div className="login-input-text">
+                    <input type="text" name="email" onChange={handleChange} value={values.email} placeholder="Email address" />
+                  </div>
+                  <div className="login-input-text">
+                    <input type="password" name="password" onChange={handleChange} value={values.password} placeholder="Password" />
+                  </div>
+                </div>
+                <div className="content-right-remember-check">
+                  <Checkbox>Remember me</Checkbox>
+                  <Link to="/forgot-password">Forgot your password?</Link>
+                </div>
+                <button type="submit" className="btn-login">Login</button>
+              </form>
+            )}
+          </Formik>
           <div className="content-right-register">
             <span>Dont't have an account?</span>
             <Link to="/register">Register</Link>
